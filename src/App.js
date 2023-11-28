9d1b356f33f39c36faa3753c3656db059cf68436
Atif Razzaq <120604563+atii007@users.noreply.github.com>
2023-11-28 23:55:43 +0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,50 @@
+import { useState } from "react";
+import Form from "./Components/Form/Form";
+import Header from "./Components/Header/Header";
+import Table from "./Components/Table/Table";
+
+function App() {
+  const [userInput, setUserInput] = useState(null);
+
+  const calculateHandler = (userInput) => {
+    setUserInput(userInput);
+  };
+  const yearlyData = [];
+
+  if (userInput) {
+    let currentSavings = +userInput["current-savings"];
+    const yearlyContribution = +userInput["yearly-contribution"];
+    const expectedReturn = +userInput["expected-return"] / 100;
+    const duration = +userInput["duration"];
+
+    for (let i = 0; i < duration; i++) {
+      const yearlyInterest = currentSavings * expectedReturn;
+      currentSavings += yearlyInterest + yearlyContribution;
+      yearlyData.push({
+        year: i + 1,
+        yearlyInterest: yearlyInterest,
+        savingsEndOfYear: currentSavings,
+        yearlyContribution: yearlyContribution,
+      });
+    }
+  }
+
+  return (
+    <div>
+      <Header />
+
+      <Form onCalculate={calculateHandler} />
+      {!userInput && (
+        <p style={{ textAlign: "center" }}>No Investments Calculated Yet.</p>
+      )}
+      {userInput && (
+        <Table
+          data={yearlyData}
+          initialInvestment={userInput["current-savings"]}
+        />
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Form/Form.js
@@ -0,0 +1,103 @@
+import { useState } from "react";
+import classes from "./Form.module.css";
+
+const Form = (props) => {
+  const initialUserInput = {
+    "current-savings": 10000,
+    "yearly-contribution": 1200,
+    "expected-return": 7,
+    duration: 10,
+  };
+
+  const [userInput, setUserInput] = useState({
+    "current-savings": 10000,
+    "yearly-contribution": 1200,
+    "expected-return": 7,
+    duration: 10,
+  });
+
+  const FormSubmitHandler = (event) => {
+    event.preventDefault();
+    props.onCalculate(userInput);
+  };
+  const FormResetHandler = (event) => {
+    setUserInput(initialUserInput);
+  };
+  const inputChangeHandler = (input, value) => {
+    setUserInput((prevInput) => {
+      return {
+        ...prevInput,
+        [input]: value,
+      };
+    });
+  };
+
+  return (
+    <form onSubmit={FormSubmitHandler} className={classes.form}>
+      <div className={classes["input-group"]}>
+        <p>
+          <label htmlFor="current-savings">Current Savings ($)</label>
+          <input
+            onChange={(event) => {
+              inputChangeHandler("current-savings", event.target.value);
+            }}
+            value={userInput["current-savings"]}
+            type="number"
+            id="current-savings"
+          />
+        </p>
+        <p>
+          <label htmlFor="yearly-contribution">Yearly Savings ($)</label>
+          <input
+            onChange={(event) => {
+              inputChangeHandler("yearly-contribution", event.target.value);
+            }}
+            value={userInput["yearly-contribution"]}
+            type="number"
+            id="yearly-contribution"
+          />
+        </p>
+      </div>
+      <div className={classes["input-group"]}>
+        <p>
+          <label htmlFor="expected-return">
+            Expected Interest (%, per year)
+          </label>
+          <input
+            onChange={(event) => {
+              inputChangeHandler("expected-return", event.target.value);
+            }}
+            value={userInput["expected-return"]}
+            type="number"
+            id="expected-return"
+          />
+        </p>
+        <p>
+          <label htmlFor="duration">Investment Duration (years)</label>
+          <input
+            onChange={(event) => {
+              inputChangeHandler("duration", event.target.value);
+            }}
+            value={userInput["duration"]}
+            type="number"
+            id="duration"
+          />
+        </p>
+      </div>
+      <p className={classes.actions}>
+        <button
+          onClick={FormResetHandler}
+          type="reset"
+          className={classes.buttonAlt}
+        >
+          Reset
+        </button>
+        <button type="submit" className={classes.button}>
+          Calculate
+        </button>
+      </p>
+    </form>
+  );
+};
+
+export default Form;
